2c498ce88dbeac589a9e5cf2f2c09e18420e43e6
Desislava Topuzakova <desislavatopuzakova@gmail.com>
2026-08-05 20:00:54 +0300

    Added new logic in orders

---
+++ orders.js
@@ -0,0 +1,10 @@
+//write a random function that generates a random order ID
+function generateRandomOrderID() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let orderID = '';
+    for (let i = 0; i < 10; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        orderID += characters[randomIndex];
+    }
+    return orderID;
+}
